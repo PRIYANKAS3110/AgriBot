6f4f827b1551fbb408b550d0c7e762c6731c683d
priyankas3110 <priyankasekarjaya@gmail.com>
2024-08-01 23:12:50 +0530

    five

--- bot.py
+++ bot.py
@@ -9,10 +9,7 @@ import os
 def configure():
     load_dotenv()
 
-
-configure()
-
-
+# spacy model for similarity checking
 nlp = spacy.load("en_core_web_md")
 genai.configure(api_key= os.getenv('API_KEY'))
 model_name = 'gemini-pro'
@@ -23,17 +20,17 @@ if 'chat_history' not in st.session_state:
 def send_message(prompt):
     try:
         model = genai.GenerativeModel(model_name)
-        
+        # formating with role and content keys
         formatted_history = [{"role": msg["role"], "content": msg["content"]} for msg in st.session_state.chat_history]
         chat = model.start_chat(history=formatted_history)
         message = {"text": prompt} 
-        
+        # response = chat.send_message(message, stream=True)
         response = chat.send_message(prompt, stream=True)
         response_text = []
         for chunk in response:
             if chunk.text:
                 response_text.append(chunk.text)
-        full_response = " ".join(response_text).replace("\n", "  \n")  
+        full_response = " ".join(response_text).replace("\n", "  \n")  # Join chunks and replace newlines with markdown newlines
         st.session_state.chat_history.append({"role": "user", "content": prompt})
         st.session_state.chat_history.append({"role": "model", "content": full_response})
         return full_response
@@ -45,10 +42,10 @@ with open('keywords.json', 'r') as f:
     keywords = json.load(f)
 
 # Main title
-st.markdown("<h1 style='text-align: center; color: green;'>Chat with AgriBot !</h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: green;'>Chat with Agri Master!</h1>", unsafe_allow_html=True)
 
 # Subheader
-st.markdown("<h3 style='text-align: center;'>Ask anything about crop and get the answer</h3>", unsafe_allow_html=True)
+st.markdown("<h3 style='text-align: center;'>Ask about crop diseases treatment and Cultivation practices</h3>", unsafe_allow_html=True)
 
 # User input section with enhanced style
 user_input = st.text_input("Ask me anything about crop diseases or cultivation practices:", key="user_input")
